c52ec46c082881bfbf78cf0f217eaa8e080a0877
spooja12364 <spooja12364@users.noreply.github.com>
2026-06-23 17:03:37 +0000

    deploy: 5f9edaaa3eca4e2a6a786aae7faa14e69adbff7a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2513090007"
+    serviceWorkerVersion: "1866686128"
   }
 });
